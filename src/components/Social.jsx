319554d637f232488c1734dc87d22b17ca7fe426
nquan7252 <81738938+nquan7252@users.noreply.github.com>
2022-11-29 12:35:39 -0800

    Update Social.jsx

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 class Social extends React.Component {
     render() { 
         return  <ul className="social">
-            <li><a href="https://www.instagram.com/_mquannn_/" target="_blank"><img src={require("../assets/instagram.png")} className="social-icon"alt =""/></a></li>
-            <li><a href="https://miwa.sbs/" target="_blank"><img src={require("../assets/www.png")} className="social-icon" alt=""/></a></li>
-            <li><a href="https://www.linkedin.com/in/nguyen-quan-299a3a224/" target="_blank"><img src={require("../assets/linkedin.png")} className="social-icon" alt=""/></a></li>
+            <li><a href="https://nquan7252.github.io/Miwawebsite/" target="_blank"><img src={require("../assets/www.png")} className="social-icon" alt=""/></a></li>
         </ul>
 
     }
